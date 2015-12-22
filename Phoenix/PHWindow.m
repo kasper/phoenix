a68f49dee299e8bfa80c0f0546832b958a88351c
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2015-12-22 14:19:25 +0000

    Improve _AXUIElementGetWindow declaration

--- Phoenix/PHWindow.m
+++ Phoenix/PHWindow.m
@@ -19,7 +19,7 @@ static NSString * const PHWindowKey = @"window";
 static NSString * const PHScoreKey = @"score";
 
 // XXX: Undocumented private API to get the CGWindowID for an AXUIElementRef
-AXError _AXUIElementGetWindow(AXUIElementRef, CGWindowID *out);
+AXError _AXUIElementGetWindow(AXUIElementRef element, CGWindowID *identifier);
 
 #pragma mark - Initialise
 
