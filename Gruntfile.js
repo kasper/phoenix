b3395045b746bc5558ad475ecfb2f8d2cf8318ed
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2018-12-26 16:00:58 +0200

    Remove grunt-contrib-uglify dependency and use UglifyJS directly

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,24 +6,7 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json')
 
-    uglify: {
-
-      library: {
-        files: {
-          'Phoenix/<%= pkg.name %>.min.js': 'Phoenix/<%= pkg.name %>.js'
-        }
-      }
-    }
   });
-
-  /* Load tasks */
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  /* Register tasks */
-
-  grunt.registerTask('build', [ 'uglify' ]);
-  grunt.registerTask('default', [ 'build' ]);
 }
