cd3944a2291fc6d92b937fa3357ecad0bbd7f9b1
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2022-01-23 21:03:34 +0200

    Use PHUserDefaultsOpenAtLoginKey instead of PHPreferencesOpenAtLoginKey

--- Phoenix/PHOpenAtLoginHelper.m
+++ Phoenix/PHOpenAtLoginHelper.m
@@ -4,18 +4,18 @@
 
 @import ServiceManagement;
 
-#import "PHPreferences.h"
 #import "PHOpenAtLoginHelper.h"
 
 @implementation PHOpenAtLoginHelper
 
+static NSString * const PHUserDefaultsOpenAtLoginKey = @"PHOpenAtLogin";
 static NSString * const PHLauncherBundleIdentifier = @"org.khirviko.Phoenix.Launcher";
 
 #pragma mark - Login Item
 
 + (BOOL) opensAtLogin {
 
-    return [[NSUserDefaults standardUserDefaults] boolForKey:PHPreferencesOpenAtLoginKey];
+    return [[NSUserDefaults standardUserDefaults] boolForKey:PHUserDefaultsOpenAtLoginKey];
 }
 
 + (void) setOpensAtLogin:(BOOL)opensAtLogin {
@@ -23,7 +23,7 @@ static NSString * const PHLauncherBundleIdentifier = @"org.khirviko.Phoenix.Laun
     BOOL didSet = SMLoginItemSetEnabled((__bridge CFStringRef) PHLauncherBundleIdentifier, opensAtLogin);
 
     if (didSet) {
-        [[NSUserDefaults standardUserDefaults] setBool:opensAtLogin forKey:PHPreferencesOpenAtLoginKey];
+        [[NSUserDefaults standardUserDefaults] setBool:opensAtLogin forKey:PHUserDefaultsOpenAtLoginKey];
     }
 }
 
