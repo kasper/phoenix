f9c0250a499303cb5ae09ed3bb0d0a2f2ae0e25e
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2024-06-10 20:25:20 +0300

    Fix prism-react-renderer theme imports to v2

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,5 +1,5 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 const config = {
   title: 'Phoenix',
